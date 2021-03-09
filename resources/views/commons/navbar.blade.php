a1c4148f990f3ffbbfb7a649485bc2a47b947723
shouma.itezono <zooonshou@gmail.com>
2021-03-09 05:47:52 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,8 +10,10 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
         </div>
     </nav>
